774149b145da91c75adb2496a2e0d9d2066a86a2
Rob <71897226+rfreemo@users.noreply.github.com>
2024-12-12 16:40:17 +0100

    Update simple_1.js
    change to ES module structure

--- simple_1.js
+++ simple_1.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
- 
-    const appElement = document.getElementById("app");
-
-
+export function initSPA(selector) {
+    const appElement = document.querySelector(selector);
     const greeting = appElement.getAttribute("data-greeting");
-
-
     appElement.innerHTML = `<h1>Hello, ${greeting}!</h1>`;
+}
+
+// Bootstrap logic
+document.addEventListener("DOMContentLoaded", () => {
+    initSPA("#app");
 });
